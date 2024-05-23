17831ad3f393a5ed124184655425c70a5b6714b2
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2024-05-23 22:13:36 +0300

    Add functionality to choose number of sides on each die

--- dice-roller.py
+++ dice-roller.py
@@ -2,24 +2,30 @@ import random
 from tqdm import tqdm
 
 
-def get_number_of_dice():
+def get_dice_configuration():
     while True:
         try:
             number_of_dice = int(input('Enter how many dice you want to roll:\n> '))
-            if number_of_dice > 0:
-                return number_of_dice
-            else:
+            if number_of_dice <= 0:
                 print('Please enter a positive integer.')
+                continue
+
+            sides_of_dice = int(input('Enter the number of sides on each die:\n> '))
+            if sides_of_dice <= 0:
+                print('Please enter a positive integer.')
+                continue
+
+            return number_of_dice, sides_of_dice
         except ValueError:
             print('Invalid input. Please enter a valid integer.')
 
 
-def simulate_rolls(number_of_dice, simulations=1000000):
-    results = {i: 0 for i in range(number_of_dice, (number_of_dice * 6) + 1)}
-    print(f'Simulating {simulations:,} rolls of {number_of_dice} dice...')
+def simulate_rolls(number_of_dice, sides_of_dice, simulations=1000000):
+    results = {i: 0 for i in range(number_of_dice, number_of_dice * sides_of_dice + 1)}
+    print(f'Simulating {simulations:,} rolls of {number_of_dice} dice with {sides_of_dice} sides each...')
 
     for _ in tqdm(range(simulations), desc="Rolling Dice"):
-        total = sum(random.randint(1, 6) for _ in range(number_of_dice))
+        total = sum(random.randint(1, sides_of_dice) for _ in range(number_of_dice))
         results[total] += 1
 
     return results
@@ -33,9 +39,9 @@ def display_results(results, simulations):
 
 
 def main():
-    number_of_dice = get_number_of_dice()
+    number_of_dice, sides_of_dice = get_dice_configuration()
     num_simulations = 1000000
-    results = simulate_rolls(number_of_dice, num_simulations)
+    results = simulate_rolls(number_of_dice, sides_of_dice, num_simulations)
     display_results(results, num_simulations)
 
 
